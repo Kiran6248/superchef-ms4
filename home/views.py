fe6c318ce68aded6f3e6200be143f29635b6e2dc
Kiran6248 <kiran.satyarthy@gmail.com>
2021-06-10 15:05:14 +0000

    Add view for new arrival on index page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    # get new arrivals to display on home page
+
+    home_new_arrivals = Product.objects.filter(category__name='new_arrivals')
+
+    context = {
+        'home_new_arrivals': home_new_arrivals
+    }
+
+    return render(request, 'home/index.html', context)
