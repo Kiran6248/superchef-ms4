2856304c77958450446bbd3ed38537f148f5929c
Kiran6248 <kiran.satyarthy@gmail.com>
2021-06-24 09:10:15 +0000

    Add view, url and template for edit blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_blog_posts, name='blogs'),
     path('detail/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('add/', views.add_blog, name='add_blog'),
+    path('edit/<slug:slug>/', views.edit_blog, name='edit_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -58,3 +58,37 @@ def add_blog(request):
     }
 
     return render(request, template, context)
+
+
+
+@login_required
+def edit_blog(request, slug):
+    """
+    Edit a blog on the blog page
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, this is only for store owners.')
+        return redirect(reverse('home'))
+    
+    blog = get_object_or_404(BlogPost, slug=slug)
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated blog')
+            return redirect(reverse('blog_detail', args=[blog.slug]))
+        else:
+            messages.error(request, 'Failed to edit blog. \
+                Please check the form is valid and try again.')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing "{blog.title}" .')
+    
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+
+    return render(request, template, context)
