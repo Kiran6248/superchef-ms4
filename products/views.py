6c045f65008c63980ddf8041d8f1fe4e40e0ca8e
Kiran6248 <kiran.satyarthy@gmail.com>
2021-06-29 10:35:19 +0000

    Correct line too log error in product app

--- products/views.py
+++ products/views.py
@@ -44,7 +44,8 @@ def all_products(request):
                                "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = (
+                Q(name__icontains=query) | Q(description__icontains=query))
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -85,7 +86,9 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, (
+                'Failed to add product. Please '
+                'check the form is valid and try again.'))
     else:
         form = ProductForm()
 
@@ -112,7 +115,9 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, (
+                'Failed to update product. Please '
+                'ensure the form is valid.'))
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
