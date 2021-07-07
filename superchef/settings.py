4c0a1fca52c21a74045788102c8589f6e1efaa8c
Kiran6248 <kiran.satyarthy@gmail.com>
2021-07-07 08:50:01 +0000

    Connect project to heroku

--- superchef/settings.py
+++ superchef/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8^k53-b)t=g)j#$k@&6-mnc&&246o+ott1j7$o-*1ra4hk6c#_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['superchef-ms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,18 @@ WSGI_APPLICATION = 'superchef.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
