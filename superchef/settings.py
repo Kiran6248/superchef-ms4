357c6d70b9ec2a18bdce51a8294d872fbb9b46c8
Kiran6248 <kiran.satyarthy@gmail.com>
2021-07-09 10:58:32 +0000

    add X frame sameorigin

--- superchef/settings.py
+++ superchef/settings.py
@@ -64,7 +64,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'superchef.urls'
 
